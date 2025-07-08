15af6ccfa71648c8e7e929a339a21d405835e192
gayatrig19 <ghogaregayatri@gmail.com>
2025-07-08 22:16:34 +0100

    Storing and accessing data dynamically with variables

---
+++ 04-variables/variables.php
@@ -0,0 +1,27 @@
+<?php
+    $title = 'PHP is amazing';
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+</head>
+
+<body>
+    <h1><?php echo $title; ?></h1>
+    <?php
+
+    $greetings = 'Hello PHP!';
+    $year = 2050;
+
+    echo $greetings;
+    echo $greetings;
+    echo $year;
+    
+    ?>
+
+</body>
+
+</html>
