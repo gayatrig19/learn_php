b87a83d93e6f769ed38114b4225d5ab177ffce85
gayatrig19 <ghogaregayatri@gmail.com>
2025-12-31 23:00:17 +0000

    Floating-point representation in php

--- 07-php-essentials/05-booleans/booleans.php
+++ 07-php-essentials/05-booleans/booleans.php
@@ -1,61 +1,71 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../01-strings/simple.css" />
     <title>Document</title>
 </head>
+
 <body>
-    <pre><?php 
+    <pre><?php
 
-    // Boolean Operators
-    var_dump(true);
-    var_dump(false);
+            // Boolean Operators
+            var_dump(true);
+            var_dump(false);
 
-    $value = true;
-    var_dump($value);
+            $value = true;
+            var_dump($value);
 
-    echo "----\n";
+            echo "----\n";
 
-    // Comparison Operators
-    $meaning = 42;
-    var_dump($meaning > 40); // true
-    var_dump(40 > 20); // true
-    var_dump((40 + 2) > 20); // true
-    var_dump((40 + 2) < 20); // false
-    var_dump((40 + 2) >= 20); // true
-    var_dump((40 + 2) <= 20); // false
+            // Comparison Operators
+            $meaning = 42;
+            var_dump($meaning > 40); // true
+            var_dump(40 > 20); // true
+            var_dump((40 + 2) > 20); // true
+            var_dump((40 + 2) < 20); // false
+            var_dump((40 + 2) >= 20); // true
+            var_dump((40 + 2) <= 20); // false
 
-    var_dump(10 < 10); // false
-    var_dump(10 <= 10); // true
+            var_dump(10 < 10); // false
+            var_dump(10 <= 10); // true
 
-    echo "----\n";
+            echo "----\n";
 
-    $name = 'Gayatri';
-    var_dump($name === 'Gayatri'); // true
-    var_dump($name !== 'Gayatri'); // false
+            $name = 'Gayatri';
+            var_dump($name === 'Gayatri'); // true
+            var_dump($name !== 'Gayatri'); // false
 
-    $age = 30;
-    var_dump($age === 30); // true
-    var_dump($age === '30'); // false
+            $age = 30;
+            var_dump($age === 30); // true
+            var_dump($age === '30'); // false
 
-    var_dump($age == '30'); // true
-    var_dump($age != '30'); // false
-    var_dump($age !== '30'); // true
+            var_dump($age == '30'); // true
+            var_dump($age != '30'); // false
+            var_dump($age !== '30'); // true
 
-    echo "----\n";
+            echo "----\n";
 
-    $unitsSold = 10;
+            $unitsSold = 10;
 
-    $maxLimit = "5";
+            $maxLimit = "5";
 
-    echo $unitsSold + $maxLimit . "\n"; // Output: 15
-    echo $unitsSold * $maxLimit . "\n"; // Output: 50
-    echo $unitsSold > $maxLimit; // Output: 1
-    echo $unitsSold . $maxLimit; // Output: 105
-    
-    ?></pre>
-    
+            echo $unitsSold + $maxLimit . "\n"; // Output: 15
+            echo $unitsSold * $maxLimit . "\n"; // Output: 50
+            echo $unitsSold > $maxLimit; // Output: 1
+            echo $unitsSold . $maxLimit; // Output: 105
+
+
+            // Floating-point representation in PHP can lead to precision issues because it uses binary approximations for decimal values, 
+            // resulting in possible rounding errors during calculations, outputs bool(false)
+            $amount = -10;
+            $conversionRate = 0.3;
+            var_dump(($amount * $conversionRate) === -3);
+
+            ?></pre>
+
 </body>
+
 </html>
